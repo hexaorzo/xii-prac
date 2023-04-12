e6e1a33980849fbfadbd2294f7d450b1eebd0abf
Rajnish Kumar Singh <hi@hexaorzo.net>
2023-04-12 16:53:16 +0000

    Added Program 2

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 A git repository for class-XII ISC practicals
 ## List of practicals
 1. [Composite Magic Integers](/practical-1/) (Number Program)
-2. [Common Words](/practical-2/) (String Program)
+2. [Number Type](/practical-2/) (Number/String Program)
---
+++ practical-2/NumberType.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class NumberType
+{
+    public static void main(String args[]) throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        String n = sc.nextLine();
+        int count = 0;
+        for(int i = 0; i < n.length(); i++)
+            if(n.charAt(i) >= '0' && n.charAt(i) <= '9')
+                count++;
+        if(count == n.length())
+        {
+            boolean isIncreasing = true;
+            boolean isDecreasing = true;
+            for(int i = 0; i < n.length() - 1; i++)
+            {
+                if(n.charAt(i) > n.charAt(i + 1))
+                    isIncreasing = false;
+                if(n.charAt(i) < n.charAt(i + 1))
+                    isDecreasing = false;
+            }
+            if(isIncreasing)
+                System.out.println(n + " IS AN INCREASING NUMBER");
+            if(isDecreasing)
+                System.out.println(n + " IS A DECREASING NUMBER");
+            if(!isIncreasing && !isDecreasing)
+                System.out.println(n + " IS A BOUNCY NUMBER");
+        }
+        else
+            System.out.println("INVALID INPUT");
+    }
+}
