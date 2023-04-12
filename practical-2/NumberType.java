4523b9c6f0fabf17c308a6cdf18cefcad303fa4c
Rajnish Kumar Singh <hi@hexaorzo.net>
2023-04-12 23:09:26 +0530

    Update NumberType.java

--- practical-2/NumberType.java
+++ practical-2/NumberType.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class NumberType
 {
-    public static void main(String args[]) throws IOException
+    public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
