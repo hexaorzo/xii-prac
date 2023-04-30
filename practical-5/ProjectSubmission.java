bda995cda9c858bdb201485e27218c16ace6bfff
Rajnish Kumar Singh <hi@hexaorzo.net>
2023-04-30 15:10:17 +0000

    update

--- practical-5/ProjectSubmission.java
+++ practical-5/ProjectSubmission.java
@@ -5,7 +5,6 @@ public class ProjectSubmission
     {
         int d = 0, m = 0, y = 0;
         String days[] = {"SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"};
-        String months[] = {"JANUARY","FEBRUARY","MARCH","APRIL","MAY","JUNE","JULY","AUGUST","SEPTEMBER","OCTOBER","NOVEMBER","DECEMBER"};
         int daysInMonth[] = {31,28,31,30,31,30,31,31,30,31,30,31};
         Scanner sc = new Scanner(System.in);
         System.out.print("ENTER PROJECT DATE: ");
