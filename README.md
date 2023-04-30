e8dadfb9e89c185c125194e3dc51e1133c107e92
Rajnish Kumar Singh <hi@hexaorzo.net>
2023-04-30 05:53:12 +0000

    Added ProjectSubmission.java

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A git repository for class-XII ISC practicals
 1. [Composite Magic Integers](/practical-1/) (Number Program)
 2. [Number Type](/practical-2/) (Number/String Program)
 3. [Insert Word](/practical-3/) (String Program)
+4. [Mirror Matrix](/practical-4/) (Matrix Program)
+5. [Project Submission Date](/practical-5/) (String/Date Program)
---
+++ practical-5/ProjectSubmission.java
@@ -0,0 +1,81 @@
+import java.util.*;
+public class ProjectSubmission
+{
+    public static void main(String args[])
+    {
+        int d = 0, m = 0, y = 0;
+        String days[] = {"SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"};
+        String months[] = {"JANUARY","FEBRUARY","MARCH","APRIL","MAY","JUNE","JULY","AUGUST","SEPTEMBER","OCTOBER","NOVEMBER","DECEMBER"};
+        String daysInMonth[] = {"31","28","31","30","31","30","31","31","30","31","30","31"};
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER PROJECT DATE: ");
+        String date = sc.nextLine().toUpperCase();
+        try
+        {
+            d = Integer.parseInt(date.substring(0,date.indexOf('/')));
+            m = Integer.parseInt(date.substring(date.indexOf('/')+1,date.lastIndexOf('/')));
+            y = Integer.parseInt(date.substring(date.lastIndexOf('/')+1));
+        }
+        catch(Exception e)
+        {
+            System.out.println("INVALID DATE");
+            System.exit(0);
+        }
+        System.out.print("DAY OF THE WEEK: ");
+        String day = sc.nextLine().toUpperCase();
+        if(m==2 && y%4==0 && y%100!=0 || y%400==0)
+            daysInMonth[1] = "29";
+        try
+        {
+            if(d>Integer.parseInt(daysInMonth[m-1]))
+            {
+                System.out.println("INVALID DATE");
+                System.exit(0);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("INVALID DAY");
+            System.exit(0);
+        }
+        d += 89;
+        while(true)
+        {
+            if (m==2 && y%4==0 && y%100!=0 || y%400==0)
+                daysInMonth[1] = "29";
+            if(d<=Integer.parseInt(daysInMonth[m-1]))
+                break;
+            d -= Integer.parseInt(daysInMonth[m-1]);
+            m++;
+            if(m>12)
+            {
+                m = 1;
+                y++;
+            }
+        }
+        int i = 0;
+        while(!day.equals(days[i]))
+            i++;
+        i+=89;
+        while(i>=7)
+            i-=7;
+        System.out.println("PROJECT SUBMISSION DATE: "+d+"/"+m+"/"+y);
+        System.out.println("DAY OF THE WEEK: "+days[i]);
+        if(days[i].equals("SUNDAY"))
+        {
+            d++;
+            if(d>Integer.parseInt(daysInMonth[m-1]))
+            {
+                d = 1;
+                m++;
+                if(m>12)
+                {
+                    m = 1;
+                    y++;
+                }
+            }
+            System.out.println("PROJECT TO BE SUBMITTED: "+d+"/"+m+"/"+y);
+            System.out.println("DAY OF THE WEEK: "+days[i+1]);
+        }
+    }
+}
