ce7678cea8ebc90933d19120b7c1b275f6b3c928
Rajnish Kumar Singh <hi@hexaorzo.net>
2023-09-03 17:07:34 +0000

    Added prac-11

--- practical-11/Combination.java
+++ practical-11/Combination.java
@@ -7,31 +7,27 @@ public class Combination
         System.out.print("N = ");
         int n = sc.nextInt();
         int m = (int)Math.pow(2, n);
-        int[][] a = new int[m][n];
-        for (int i = 0; i < m; i++) 
-        {
-            int k = i;
-            for (int j = n - 1; j >= 0; j--) 
-            {
-                if (k % 2 == 0)
-                    a[i][j] = j+1;
-                else
-                    a[i][j] = -(j+1);
-                k /= 2;
-            }
-        }
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = (i+1);
         int f = 0;
         for (int i = 0; i < m; i++) 
         {
             int sum = 0;
             for (int j = 0; j < n; j++) 
-                sum += a[i][j];
+                sum += a[j];
             if (sum == 0) 
             {
                 for (int j = 0; j < n; j++) 
-                    System.out.print(a[i][j] + "\t");
+                    System.out.print(a[j] + "\t");
                 System.out.println();
                 f++;
+            }
+            for (int j = n-1; j >= 0; j--)
+            {
+                a[j] = -a[j];
+                if (a[j] < 0)
+                    break;
             }
         }
         System.out.println("Total number of combinations = " + f);
