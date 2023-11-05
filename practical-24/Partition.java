9a1693ddfd6876ca55e84714073c8b94961c264d
Rajnish Kumar Singh <hi@hexaorzo.net>
2023-11-05 20:05:33 +0000

    Added prac-24

---
+++ practical-24/Partition.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Partition 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        partition(n, n-1, "");
+
+    }
+    public static void partition(int n, int m, String p) 
+    {
+        if (n == 0) 
+        {
+            System.out.println(p);
+            return;
+        }
+
+        for (int i = Math.min(m, n); i >= 1; i--) 
+            partition(n-i, i, p + i + " " );
+    }
+}
