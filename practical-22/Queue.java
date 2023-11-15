83ecdcffb5be0f645f54988c022c0ac0e269ac6a
Restyled.io <commits@restyled.io>
2023-11-15 07:45:31 +0000

    Restyled by clang-format

--- practical-22/Queue.java
+++ practical-22/Queue.java
@@ -1,77 +1,66 @@
 import java.util.*;
-class Queue 
-{
-    String Q[]; int f, r, s;
-    Queue(int n)
-    {
-        s = n;
-        Q = new String[s];
+class Queue {
+  String Q[];
+  int f, r, s;
+  Queue(int n) {
+    s = n;
+    Q = new String[s];
+    f = r = -1;
+  }
+  void push(String x) {
+    if (r == s - 1)
+      System.out.println("Queue Overflow");
+    else {
+      if (f == -1)
+        f = r = 0;
+      else
+        r++;
+      Q[r] = x;
+    }
+  }
+  void pop() {
+    if (f == -1 && r == -1)
+      System.out.println("Queue Underflow");
+    else {
+      System.out.println("Deleted: " + Q[f]);
+      if (f == r)
         f = r = -1;
+      else
+        f++;
     }
-    void push(String x)
-    {
-        if (r == s - 1)
-            System.out.println("Queue Overflow");
-        else
-        {
-            if (f == -1)
-                f = r = 0;
-            else
-                r++;
-            Q[r] = x;
-        }
+  }
+  void display() {
+    if (f == -1 && r == -1)
+      System.out.println("Queue is empty");
+    else {
+      System.out.println("Queue: ");
+      for (int i = f; i <= r; i++)
+        System.out.print(Q[i] + " ");
+      System.out.println();
     }
-    void pop()
-    {
-        if (f == -1 && r == -1)
-            System.out.println("Queue Underflow");
-        else
-        {
-            System.out.println("Deleted: " + Q[f]);
-            if (f == r)
-                f = r = -1;
-            else
-                f++;
-        }
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter size of queue: ");
+    int n = sc.nextInt();
+    Queue q = new Queue(n);
+    while (true) {
+
+      System.out.println("\n1. Insert an element");
+      System.out.println("2. Delete an element");
+      System.out.println("3. Display the elements");
+      System.out.print("Enter choice: ");
+      char c = sc.next().charAt(0);
+      if (c == '1') {
+        System.out.print("Enter element: ");
+        String x = sc.next();
+        q.push(x);
+      } else if (c == '2')
+        q.pop();
+      else if (c == '3')
+        q.display();
+      else
+        break;
     }
-    void display()
-    {
-        if (f == -1 && r == -1)
-            System.out.println("Queue is empty");
-        else
-        {
-            System.out.println("Queue: ");
-            for (int i = f; i <= r; i++)
-                System.out.print(Q[i] + " ");
-            System.out.println();
-        }
-    }
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter size of queue: ");
-        int n = sc.nextInt();
-        Queue q = new Queue(n);
-        while (true)
-        {
-            
-            System.out.println("\n1. Insert an element");
-            System.out.println("2. Delete an element");
-            System.out.println("3. Display the elements");
-            System.out.print("Enter choice: ");
-            char c = sc.next().charAt(0);
-            if(c=='1')
-            {
-                System.out.print("Enter element: ");
-                String x = sc.next();
-                q.push(x);
-            }
-            else if(c=='2')
-                q.pop();
-            else if(c=='3')
-                q.display();
-            else
-                break;
-        }
-    }
+  }
 }
--- practical-23/Time.java
+++ practical-23/Time.java
@@ -1,44 +1,43 @@
 import java.util.*;
-public class Time 
-{
-    public static void main(String[] args) 
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Time: ");
-        String t = sc.nextLine();
-        int h=0, m=0; String ms=""; 
-        try
-        {
-            h = Integer.parseInt(t.substring(0, t.indexOf(',')).trim());
-            ms = t.substring(t.indexOf(',') + 1).trim();
-            m = Integer.parseInt(ms);
-            if (h <  1 || h > 12 || m < 0 || m > 59) 
-                throw new Exception();
-        }
-        catch(Exception e)
-        {
-            System.out.println("Incorrect input");
-            System.exit(0);
-        }
-        if (ms.length() == 1)
-            ms = "0" + ms;
-        System.out.print(h+":"+ms+"\t");
-        String[] n = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", 
-                        "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", 
-                        "Seventeen", "Eighteen", "Nineteen", "Twenty", "Twenty one", "Twenty two",
-                        "Twenty three", "Twenty four", "Twenty five", "Twenty six", "Twenty seven",
-                        "Twenty eight", "Twenty nine"};
-        if(m==0)
-            System.out.println(n[h-1]+" o' clock");
-        else if(m==15)
-            System.out.println("Quarter past "+n[h-1].toLowerCase());
-        else if(m<30)
-            System.out.println(n[m-1]+" minutes past "+n[h-1].toLowerCase());
-        else if(m==30)
-            System.out.println("Half past "+n[h-1].toLowerCase());
-        else if(m==45)
-            System.out.println("Quarter to "+n[h%12].toLowerCase());
-        else if(m>30)
-            System.out.println(n[59-m]+" minutes to "+n[h%12].toLowerCase());
+public class Time {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Time: ");
+    String t = sc.nextLine();
+    int h = 0, m = 0;
+    String ms = "";
+    try {
+      h = Integer.parseInt(t.substring(0, t.indexOf(',')).trim());
+      ms = t.substring(t.indexOf(',') + 1).trim();
+      m = Integer.parseInt(ms);
+      if (h < 1 || h > 12 || m < 0 || m > 59)
+        throw new Exception();
+    } catch (Exception e) {
+      System.out.println("Incorrect input");
+      System.exit(0);
     }
+    if (ms.length() == 1)
+      ms = "0" + ms;
+    System.out.print(h + ":" + ms + "\t");
+    String[] n = {"One",         "Two",        "Three",        "Four",
+                  "Five",        "Six",        "Seven",        "Eight",
+                  "Nine",        "Ten",        "Eleven",       "Twelve",
+                  "Thirteen",    "Fourteen",   "Fifteen",      "Sixteen",
+                  "Seventeen",   "Eighteen",   "Nineteen",     "Twenty",
+                  "Twenty one",  "Twenty two", "Twenty three", "Twenty four",
+                  "Twenty five", "Twenty six", "Twenty seven", "Twenty eight",
+                  "Twenty nine"};
+    if (m == 0)
+      System.out.println(n[h - 1] + " o' clock");
+    else if (m == 15)
+      System.out.println("Quarter past " + n[h - 1].toLowerCase());
+    else if (m < 30)
+      System.out.println(n[m - 1] + " minutes past " + n[h - 1].toLowerCase());
+    else if (m == 30)
+      System.out.println("Half past " + n[h - 1].toLowerCase());
+    else if (m == 45)
+      System.out.println("Quarter to " + n[h % 12].toLowerCase());
+    else if (m > 30)
+      System.out.println(n[59 - m] + " minutes to " + n[h % 12].toLowerCase());
+  }
 }
