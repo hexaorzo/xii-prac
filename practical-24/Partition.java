c4212669748d6eabdc5b334cc8454a0d77b068ce
Restyled.io <commits@restyled.io>
2023-11-15 07:52:06 +0000

    Restyled by clang-format

--- practical-24/Partition.java
+++ practical-24/Partition.java
@@ -1,21 +1,17 @@
 import java.util.*;
-class Partition 
-{
-    public static void main(String[] args) 
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        int n = sc.nextInt();
-        part(n, n, "");
+class Partition {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the number: ");
+    int n = sc.nextInt();
+    part(n, n, "");
+  }
+  static void part(int n, int m, String p) {
+    if (n == 0) {
+      System.out.println(p);
+      return;
     }
-    static void part(int n, int m, String p) 
-    {
-        if (n == 0) 
-        {
-            System.out.println(p);
-            return;
-        }
-        for (int i = Math.min(m, n); i >= 1; i--) 
-            part(n-i, i, p + i + " " );
-    }
+    for (int i = Math.min(m, n); i >= 1; i--)
+      part(n - i, i, p + i + " ");
+  }
 }
