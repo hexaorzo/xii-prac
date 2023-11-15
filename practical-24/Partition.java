51c8ddafe5a6686730b648e806418bed40dfe342
Rajnish Kumar Singh <hi@hexaorzo.net>
2023-11-15 07:51:58 +0000

    update

--- practical-24/Partition.java
+++ practical-24/Partition.java
@@ -1,17 +1,21 @@
 import java.util.*;
-class Partition {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the number: ");
-    int n = sc.nextInt();
-    partition(n, n - 1, "");
-  }
-  static void partition(int n, int m, String p) {
-    if (n == 0) {
-      System.out.println(p);
-      return;
+class Partition 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        part(n, n, "");
     }
-    for (int i = Math.min(m, n); i >= 1; i--)
-      partition(n - i, i, p + i + " ");
-  }
+    static void part(int n, int m, String p) 
+    {
+        if (n == 0) 
+        {
+            System.out.println(p);
+            return;
+        }
+        for (int i = Math.min(m, n); i >= 1; i--) 
+            part(n-i, i, p + i + " " );
+    }
 }
